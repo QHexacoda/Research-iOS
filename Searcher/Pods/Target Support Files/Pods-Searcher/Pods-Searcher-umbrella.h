031dc7293cb812c676eabf95d3ed839f2a69f68f
QHexacoda <weerayoot@hexacoda.com>
2016-07-24 19:08:27 +0700

    - Added : Project files and CocoaPods.

---
+++ Searcher/Pods/Target Support Files/Pods-Searcher/Pods-Searcher-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SearcherVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SearcherVersionString[];
+
---
+++ Searcher/Pods/Target Support Files/Pods-SearcherTests/Pods-SearcherTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearcherTests : NSObject
+@end
+@implementation PodsDummy_Pods_SearcherTests
+@end
